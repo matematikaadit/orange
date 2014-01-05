3c6bd46f718a317433fdef5625fe18cdbf6f7018
Adit Cahya Ramadhan <matematika.adit@gmail.com>
2014-01-05 22:54:35 +0700

    Linked list basic in Go lang

---
+++ sequence-type/linked-list/go/list.go
@@ -0,0 +1,37 @@
+package list
+
+type Node struct {
+	next *Node
+	data int
+}
+
+type List struct {
+	head *Node
+}
+
+func (list *List) Push(x int) {
+	newNode := &Node{next: list.head, data: x}
+	list.head = newNode
+}
+
+func (list *List) Pop() (x int) {
+	if list.head == nil {
+		panic("Popping empty List")
+	}
+
+	x = list.head.data
+	list.head = list.head.next
+	return
+}
+
+func (list *List) String() string {
+	str := "{"
+	for cur := list.head; cur != nil; cur = cur.next {
+		str += strconv.Itoa(cur.data)
+		if cur.next != nil {
+			str += " "
+		}
+	}
+	str += "}"
+	return str
+}
